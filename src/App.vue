b8e7f1c27a59740200e5c37553e7e86295995108
quantgamer <thealevelphysicist@gmail.com>
2024-12-16 21:40:51 +0100

    Simple change to test gh-pages

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Well done!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
