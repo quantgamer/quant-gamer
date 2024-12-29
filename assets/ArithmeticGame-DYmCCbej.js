84f7a81d49ea5fc78a8ddb1ccd79b6ba641ad218
quantgamer <thealevelphysicist@gmail.com>
2024-12-29 22:32:31 +0000

    Updates

--- assets/ArithmeticGame-DYmCCbej.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as a,a as s,b as n,w as r,r as c,o as i,d as _}from"./index-gVza90gm.js";const m={},d={class:"arithmeticgame"};function l(f,e){const t=c("RouterLink");return i(),a("div",d,[s("nav",null,[n(t,{to:"/"},{default:r(()=>e[0]||(e[0]=[_("Home")])),_:1})])])}const p=o(m,[["render",l]]);export{p as default};
