68770f93e22fffcd84e9940573c6a9877560af31
quantgamer <thealevelphysicist@gmail.com>
2024-12-29 22:32:10 +0000

    create navigation between settings and game started

--- src/views/ArithmeticGame.vue
+++ src/views/ArithmeticGame.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const gameStarted = ref(true);
+const switchGameStarted = () => {
+  gameStarted.value = !gameStarted.value;
+};
+</script>
+
 <template>
   <div class="arithmeticgame">
+    <div class="game-container">
+      <div v-if="!gameStarted" class="settings">settings</div>
+      <div v-else class="game">game</div>
+      <button @click="switchGameStarted" />
+    </div>
     <nav>
       <RouterLink to="/">Home</RouterLink>
     </nav>
