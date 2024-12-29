f059fc77c8b633c4e29e6407f04c55b671403b19
quantgamer <thealevelphysicist@gmail.com>
2024-12-29 20:56:59 +0000

    add navigation between home page and arithmetic game page

---
+++ src/views/ArithmeticGame.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="arithmeticgame">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<style>
+.arithmeticgame {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <nav>
+      <RouterLink to="/arithmeticgame">Arithmetic Game</RouterLink>
+    </nav>
   </main>
 </template>
